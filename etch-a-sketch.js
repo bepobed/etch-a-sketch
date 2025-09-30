d394a888dd8c2cafdd00cf18d0162f27dbbd6e6f
pepob <25570982+bepobed@users.noreply.github.com>
2025-09-30 16:46:53 +0700

    Create function prompting grid size
    Small issue when the user cancels prompt, doesn't exit loop

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,23 +1,50 @@
-//selectors
-const container = document.querySelector("body");
+const body = document.querySelector("body");
+const gridButton = document.getElementById("gridButton");
 
 //create a box for the grid
 const box = document.createElement("div");
 box.classList.add("box");
-container.appendChild(box);
+body.appendChild(box);
 
 function createGrid() {
   for (let i = 0; i < 16; i++) {
     const div = document.createElement("div");
-    div.classList.add("div");
+    div.classList.add("div"); //used for display flex
     box.appendChild(div);
-    for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+      //second loop to create 16 div per row
       const subDiv = document.createElement("div");
       subDiv.classList.add("subDiv");
       div.appendChild(subDiv);
-      subDiv.textContent = i + 1;
+      subDiv.textContent = j + 1;
     }
   }
 }
-
 createGrid();
+
+//prompts user a grid size
+function promptGrid() {
+  let gridSize = Number(
+    prompt("Which size do you prefer? Enter a value between 10 to 100")
+  );
+  while (gridSize < 10 || gridSize > 100 || !Number.isInteger(gridSize)) {}
+  gridSize = Number(prompt("Please enter a valid size: 10-100"));
+}
+
+gridButton.addEventListener("click", () => {
+  promptGrid();
+});
+
+//selector after creater grid
+//have to call the function before, otherwise nodeList is empty
+const subDiv = document.querySelectorAll(".subDiv");
+
+subDiv.forEach((subDiv) => {
+  subDiv.addEventListener("mouseover", () => {
+    subDiv.style.backgroundColor = "antiquewhite";
+  });
+
+  subDiv.addEventListener("mouseout", () => {
+    subDiv.style.backgroundColor = ""; // clears inline style
+  });
+});
