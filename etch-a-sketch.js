3cb86de584f3ceb679c275135091a81047537a08
pepob <25570982+bepobed@users.noreply.github.com>
2025-09-30 18:14:42 +0700

    customGrid() is now functional
    The prompt is being called inside customGrid()
    Removed parameter

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -30,18 +30,20 @@ function promptGrid() {
   while (gridSize < 10 || gridSize > 100 || !Number.isInteger(gridSize)) {
     gridSize = Number(prompt("Please enter a valid size: 10-100"));
     //issue when user cancels prompt, doesn't exit loop
-    return gridSize;
   }
+  return gridSize;
 }
 
 //runs when user clicks on gridButton
-function customGrid(userPrompt) {
+function customGrid() {
+  let userChoice = promptGrid();
+  console.log(userChoice);
   box.replaceChildren(); //clears the box
-  for (let i = 0; i < userPrompt; i++) {
+  for (let i = 0; i < userChoice; i++) {
     const div = document.createElement("div");
     div.classList.add("div"); //used for display flex
     box.appendChild(div);
-    for (let j = 0; j < userPrompt; j++) {
+    for (let j = 0; j < userChoice; j++) {
       //second loop to create 16 div per row
       const subDiv = document.createElement("div");
       subDiv.classList.add("subDiv");
@@ -52,7 +54,6 @@ function customGrid(userPrompt) {
 }
 
 gridButton.addEventListener("click", () => {
-  promptGrid();
   customGrid();
 });
 
